d5254db5c56650289b5369b1b4c10c030d2395d6
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-03-19 17:29:02 +0600

    Feat: while loop

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,15 @@ fn while_loop(){
       }
 }
 
+fn for_loop(){
+      for y in 2..14 {
+      if y == 3 { continue;}
+      if y == 9 { break;}
+            println!(" y = {}", y);
+      }
+}
 fn main() {
     while_loop();
+    for_loop();
 }
 
