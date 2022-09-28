1009a349b9c4eb86dc1698b9ab25d7d8adc1ae6c
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-09-28 14:56:02 +0600

    ownership and borrowing concept implement

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-//! cargo documentation example
 
-///
-fn main() {
- let result = add(100, 2320);
-    println!("The result is {}", result);
-}
-
-fn add(number_one: u64, number_two: u64) -> u64 {
- number_one + number_two
+fn main (){
+    let message = String::from("Hello, world!");
+    #[warn(unused_variables)]
+    let message_2 = &message;
+    println!("{}", message);
 }
