969dd212cad35c11bf39c8d69659a779f301fbec
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-09-28 10:46:52 +0600

    return value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! cargo documentation example
+
+///
 fn main() {
-    println!("Hello, world!");
+ let result = add(100, 2320);
+    println!("The result is {}", result);
+}
+
+fn add(number_one: u64, number_two: u64) -> u64 {
+ number_one + number_two
 }
