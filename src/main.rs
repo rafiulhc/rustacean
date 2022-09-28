872ec609416e38ac13e70ab2668f423dafe6beef
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-09-28 17:15:33 +0600

    implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 
+
+struct Number {
+    number_one: u64,
+    number_two: u64,
+}
+
+impl Number {
+    fn impl_function(&self) {
+        println!(" Total is {} ", self.number_one + self.number_two);
+    }
+}
+
 fn main (){
-    let message = String::from("Hello, world!");
-    #[warn(unused_variables)]
-    let message_2 = &message;
-    println!("{}", message);
+    let new_number = Number{
+        number_one: 10,
+        number_two: 20,
+    };
+    new_number.impl_function();
 }
