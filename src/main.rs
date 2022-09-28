0e3c608cbabe5eeaa5311995e6c663ac4e4fadb9
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-09-28 20:56:16 +0600

    library implemented

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
-
-
-struct Number {
-    number_one: u64,
-    number_two: u64,
-}
-
-impl Number {
-    fn impl_function(&self) {
-        println!(" Total is {} ", self.number_one + self.number_two);
-    }
-}
-
+use rustacean::Number;
+use rustacean::SimpleTrait;
 fn main (){
     let new_number = Number{
         number_one: 10,
         number_two: 20,
     };
-    new_number.impl_function();
+    new_number.add();
 }
